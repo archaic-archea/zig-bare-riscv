0f45cc5d99be49c2188982b7a7c77e3193c82803
Lilly <fermium@anarchist.gay>
2023-03-09 13:17:22 -0500

    seperated the files a bit

--- src/entry.s
+++ src/entry.s
@@ -15,10 +15,25 @@ _entry:
         addi t0, t0, 8
         j bss_zero*/
 
+    lla t0, int
+    csrw stvec, t0
+
     enter_kern:
         j kmain
 
     # Some error occured, and we somehow returned here, just halt loop
-    hlt_loop:
-        wfi
-        j hlt_loop
+    j hlt_loop
+
+int:
+    jal trap_handler
+
+    j hlt_loop
+
+hlt_loop:
+    wfi
+    j hlt_loop
+
+write_char:
+    li t0, 0x10000000
+    sb a0, 0(t0)
+    ret
